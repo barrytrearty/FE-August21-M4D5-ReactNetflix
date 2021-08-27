478269926b47685ac40b01cd08e26e4f59b0c174
barrytrearty <btrearty@gmail.com>
2021-08-27 12:13:21 +0100

    Movies from API

--- reactnetflix/src/components/MoviesRow.jsx
+++ reactnetflix/src/components/MoviesRow.jsx
@@ -3,15 +3,33 @@ import { Container, Row } from "react-bootstrap";
 import SingleMovie from "./SingleMovie";
 
 class MoviesRow extends Component {
+  state = {
+    movies: [],
+  };
+
+  componentDidMount = async () => {
+    try {
+      let response = await fetch(
+        "http://www.omdbapi.com/?apikey=4d0dfb28&s=harry%20potter"
+      );
+      let moviesArray = await response.json();
+      this.setState({
+        movies: moviesArray.Search,
+      });
+      console.log(this.state.movies);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
     return (
       <Container className="container-fluid mb-4">
-        <h2>Movie Row</h2>
+        <h2>Harry Potter</h2>
         <Row className="row no-gutter row-cols-1 row-cols-sm-2row-cols-md-3 row-cols-lg-6">
-          <SingleMovie />
-          <SingleMovie />
-          <SingleMovie />
-          <SingleMovie />
+          {this.state.movies.map((movie) => (
+            <SingleMovie movieObj={movie} />
+          ))}
         </Row>
       </Container>
     );
--- reactnetflix/src/components/SingleMovie.jsx
+++ reactnetflix/src/components/SingleMovie.jsx
@@ -1,13 +1,17 @@
 import { Component } from "react";
 import { Col } from "react-bootstrap";
-import poster from "../data/pic3.png";
+// import poster from "../data/pic3.png";
 
 class SingleMovie extends Component {
   render() {
     return (
-      <Col>
-        <a href="#_id}">
-          <img src={poster} className="img-fluid w-100" />
+      <Col key={this.props.movieObj.imdbID}>
+        <a href="#Home">
+          <img
+            src={this.props.movieObj.Poster}
+            alt=""
+            className="img-fluid w-100"
+          />
         </a>
       </Col>
     );
