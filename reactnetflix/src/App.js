f1a04a36c96c119f142e7fa1daa141c5bdac9f90
barrytrearty <btrearty@gmail.com>
2021-08-27 15:01:00 +0100

    search row

--- reactnetflix/src/App.js
+++ reactnetflix/src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import SingleMovie from "./components/SingleMovie";
 import MoviesRow from "./components/MoviesRow";
+import SearchRow from "./components/SearchRow";
 
 function App() {
   return (
     <div className="body-div">
       <NetflixNavbar />
+      <SearchRow />
       <MoviesRow rowTitle="Harry Potter" rowQuery="harry%20potter" />
       <MoviesRow rowTitle="Lord of the Rings" rowQuery="lord%20of%20rings" />
       <MoviesRow rowTitle="Batman" rowQuery="batman" />
--- reactnetflix/src/components/NetflixNavbar.jsx
+++ reactnetflix/src/components/NetflixNavbar.jsx
@@ -1,47 +1,47 @@
-import { Navbar, Form, FormControl, Nav } from "react-bootstrap";
+import { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
 import logo from "../data/netflix_logo.png";
 
-const NetflixNavbar = () => {
-  return (
-    <Navbar variant="dark">
-      <Navbar.Brand>
-        <a href="#home">
-          <img
-            src={logo}
-            width="80rem"
-            alt=""
-            className="logoPic navbar-brand"
-          />
-        </a>
-      </Navbar.Brand>
-      <Nav className="mr-auto">
-        <a className="nav-link active" href="#home">
-          Home <span className="sr-only">(current)</span>
-        </a>
-        <a className="nav-link" href="#tvshows">
-          TV Shows
-        </a>
-        <a className="nav-link" href="#movies">
-          Movies
-        </a>
-        <a className="nav-link" href="#recentlyAdded">
-          Recently Added
-        </a>
-        <a className="nav-link" href="#mylist">
-          My List
-        </a>
-      </Nav>
-      <Form inline>
-        <FormControl
-          bg="dark"
-          variant="light"
-          type="text"
-          placeholder="Search"
-          className="mr-sm-2"
-        />
-      </Form>
-    </Navbar>
-  );
-};
+class NetflixNavbar extends Component {
+  state = {
+    searchQuery: "",
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar variant="dark">
+          <Navbar.Brand>
+            <a href="#home">
+              <img
+                src={logo}
+                width="80rem"
+                alt=""
+                className="logoPic navbar-brand"
+              />
+            </a>
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <a className="nav-link active" href="#home">
+              Home <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-link" href="#tvshows">
+              TV Shows
+            </a>
+            <a className="nav-link" href="#movies">
+              Movies
+            </a>
+            <a className="nav-link" href="#recentlyAdded">
+              Recently Added
+            </a>
+            <a className="nav-link" href="#mylist">
+              My List
+            </a>
+          </Nav>
+        </Navbar>
+      </>
+    );
+  }
+}
 
 export default NetflixNavbar;
