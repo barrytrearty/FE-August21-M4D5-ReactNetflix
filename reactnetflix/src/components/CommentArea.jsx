267389b48811df3e0734500e0ea03c824a58991d
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-08-27 13:38:32 +0200

    added selected movie comment area

--- reactnetflix/src/components/CommentArea.jsx
+++ reactnetflix/src/components/CommentArea.jsx
@@ -1,35 +1,50 @@
-import { Component } from "react";
-// import CommentList from "./CommentList";
+import { Component } from 'react'
+// import CommentList from './CommentList'
+// import AddComment from './AddComment'
+// import Loading from './Loading'
+// import Error from './Error'
 
 class CommentArea extends Component {
 
     state = {
-        comments: []
+        comments: [],
+        isLoading: true,
+        isError: false
     }
 
     componentDidMount = async () => {
         try {
-            let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/', {
+            let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + this.props.asin, {
                 headers: {
                     Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFlNGYyYTA2MTBjYjAwMTVkYTJhOGYiLCJpYXQiOjE2MzAwNDMwODMsImV4cCI6MTYzMTI1MjY4M30.TPjoSP3gBlKn79MxSZFiuU6Y6dEKPqMQplirBYINzlk"
                 }
             })
-            let comments = await response.json()
-            this.setState({
-                comments: comments
-            })
             console.log(response)
+            if (response.ok) {
+                let comments = await response.json()
+                this.setState({ comments: comments, isLoading: false, isError: false })
+            } else {
+                console.log('error')
+                this.setState({ isLoading: false, isError: true })
+            }
         } catch (error) {
-
+            console.log(error)
+            this.setState({ isLoading: false, isError: true })
         }
     }
 
     render() {
         return (
             <div>
-                {/* <CommentList commentsToShow={this.state.comments} /> */}
-            </div>)
+
+                Comment Area
+                {/* {this.state.isLoading && <Loading />}
+                {this.state.isError && <Error />}
+                <AddComment asin={this.props.asin} />
+                <CommentList commentsToShow={this.state.comments} /> */}
+            </div>
+        )
     }
 }
 
-export default CommentArea;
+export default CommentArea
--- reactnetflix/src/components/SingleMovie.jsx
+++ reactnetflix/src/components/SingleMovie.jsx
@@ -1,20 +1,30 @@
 import { Component } from "react";
 import { Col } from "react-bootstrap";
 // import poster from "../data/pic3.png";
+import CommentArea from "./CommentArea";
 
 class SingleMovie extends Component {
+
+  state = {
+    slected: false
+  }
   render() {
     return (
-      <Col key={this.props.movieObj.imdbID}>
-        <a href="#Home">
-          <img
-            src={this.props.movieObj.Poster}
-            alt=""
-            className="img-fluid w-100"
-          />
-        </a>
-      </Col>
-    );
+      <>
+        <Col key={this.props.movieObj.imdbID} onClick={() => this.setState({ selected: !this.state.selected })}
+          style={{ border: this.state.selected ? '3px solid green' : 'none' }}>
+          <a href="#Home">
+            <img
+              src={this.props.movieObj.Poster}
+              alt=""
+              className="img-fluid w-100"
+            />
+          </a>
+        </Col>
+        {
+          this.state.selected && <CommentArea asin={this.props.movieObj.imdbID} />
+        }
+      </>);
   }
 }
 
